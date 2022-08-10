d0ba7f40fa7d6476e68df118d483d675216af9bc
Sima2204 <vlad.simuta@gmail.com>
2022-08-10 22:55:19 +0300

    init: sulution added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {    }
 
     setRange(min, max) {
-
+        this.from = min;
+        this.till = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.from + this.till) / 2 );
+        return this.middle;
     }
 
     lower() {
-
+        this.till = this.middle;
     }
 
     greater() {
-
+        this.from = this.middle;
     }
 }
 
